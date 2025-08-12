82dd07ba474f355722ffede352e69f0f1a2440c7
Ser2063 <jca@mail.ru>
2025-08-12 19:21:48 +0300

    добавлен Слоупок конфликт

--- src/main/java/ru/qa2063/Main.java
+++ src/main/java/ru/qa2063/Main.java
@@ -2,7 +2,7 @@ package ru.qa2063;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello QA GURU!");
+        System.out.printf("Hello QA GURU конфликт!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
